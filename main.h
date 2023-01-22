262e939f26fc74cb7fa7e6792a44c7d36777d7f7
damiso <damiso15@yahoo.com>
2023-01-22 09:19:00 -0800

    Initial printf function

--- main.h
+++ main.h
@@ -1,6 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* normal standard c library */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struct _print - a structure to define our printf value types
+ * @print_type: the type to print
+ * @variadic_function: the function to print
+ */
+typedef struct print_type
+{
+	char *type;
+	int (*variadic_function)(va_list);
+} print_f;
+
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
