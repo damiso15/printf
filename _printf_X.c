744588f829498f5b3ae70698a5dff89ba81fd98d
Kizito24 <kizitochiazor@gmail.com>
2023-01-23 07:29:01 -0800

    What one has not experienced, one will never understand in print

---
+++ _printf_X.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _printf_x - a function that prints lowercase hexadecimal to stdout
+ * @ap: the input character to convert to hexadecimal
+ * Return: the counter
+ */
+int _printf_x(va_list ap)
+{
+	unsigned int arr[8], num1, num2, num3, sum;
+	char hex;
+	int counter;
+
+	num1 = va_arg(ap, unsigned int);
+	hex = 'R' - ':', num2 = 268435456;
+	arr[0] = num1 / num2;
+
+	for (num3 = 1; num3 < 8; num3++)
+	{
+		num2 /= 16;
+		arr[num3] = (num1 / num2) % 16;
+	}
+
+	for (num3 = 0; num3 < 8; num3++)
+	{
+		sum += arr[num3];
+		if (sum || num3 == 7)
+		{
+			if (arr[num3] < 10)
+				_putchar('0' + arr[num3]);
+			else
+				_putchar('0' + hex + arr[num3]);
+			counter++;
+		}
+	}
+	return (counter);
+}
