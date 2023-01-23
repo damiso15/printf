992b3fdb756051f6f79135a1f80cc0e437c69acc
Kizito24 <kizitochiazor@gmail.com>
2023-01-23 07:23:34 -0800

    With a face like mine, I do better in print

---
+++ _printf_u.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _printf_u - a function that prints an usigned int to stdout
+ * @ap: an unsigned int to print
+ * Return: the total number of integer
+ */
+int _printf_u(va_list ap)
+{
+	unsigned int num1;
+	int num2, counter;
+
+	num1 = va_arg(ap, unsigned int), num2 = 1;
+
+	while ((num1 / num2) > 9)
+		num2 *= 10;
+
+	while (num2 != 0)
+	{
+		counter += _putchar(num1 / num2 + '0');
+		num1 %= num2;
+		num2 /= 10;
+	}
+	return (counter);
+}
