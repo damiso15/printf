9895c2b9e3971a123bcc1441a84259b6553f17fc
damiso <damiso15@yahoo.com>
2023-01-23 05:06:43 -0800

    1. Education is when you read the fine print. Experience is what you get if you don't

---
+++ _printf_i.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _printf_i - a function that prints an integer in base 10 to stdout
+ * @ap: the base 10 integer to print
+ * Return: the total number of the integer
+ */
+int _printf_i(va_list ap)
+{
+	int arr[10], num1, num2, num3, sum, counter;
+
+	num3 = va_arg(ap, int), counter = 0;
+
+	if (num3 < 0)
+	{
+		num3 *= -1;
+		_putchar('-'), counter++;
+	}
+
+	num2 = 1000000000;
+	arr[0] = num3 / num2;
+
+	for (num1 = 1; num1 < 10; num1++)
+	{
+		num2 /= 10;
+		arr[num1] = (num3 / num2) % 10;
+	}
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		sum = 0;
+		sum += arr[num1];
+		if (sum != 0 || num1 == 9)
+		{
+			_putchar('0' + arr[num1]);
+			counter++;
+		}
+	}
+	return (counter);
+}
