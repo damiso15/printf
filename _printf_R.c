7492017798c95fa3e211c7ae003b1c718b54a6d5
Kizito24 <kizitochiazor@gmail.com>
2023-01-24 04:36:19 -0800

    The flood of print has turned reading into a process of gulping rather than savouring

---
+++ _printf_R.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _printf_R - a function that prints strings in rot13 format to stdout
+ * @ap: the string to convert to rot13 format
+ * Return: counter
+ */
+int _printf_R(va_list ap)
+{
+	unsigned int num1, num2;
+	int counter;
+	char *string;
+	char normal_str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char cnvt_str[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	string = va_arg(ap, char *);
+
+	if (string == NULL)
+		string = "(nil)";
+
+	for (num1 = 0; string[num1]; num1++)
+	{
+		for (num2 = 0; normal_str[num2]; num2++)
+		{
+			if (normal_str[num2] == string[num1])
+			{
+				_putchar(cnvt_str[num2]);
+				counter++;
+				break;
+			}
+		}
+		if (!normal_str[num2])
+		{
+			_putchar(string[num1]);
+			counter++;
+		}
+	}
+	return (counter);
+}
