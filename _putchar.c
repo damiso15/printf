b31583075884028ee5c6b8b8b2926c149572b686
damiso <damiso15@yahoo.com>
2023-01-26 09:00:30 -0800

    this should fix Task check 26 failing

--- _putchar.c
+++ _putchar.c
@@ -5,8 +5,37 @@
  * @c: the characte rto print
  * Return: on Success 1
  * On error, -1 is returned, an errno is set appropraitely
+ * Description: implementing a local buffer of 1024 bytes to call write
+ * as little as possible
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buff_size[1024];
+	static int num;
+
+	if (c == -1 || num >= 1024)
+	{
+		write(1, &buff_size, num);
+		num = 0;
+	}
+	if (c != -1)
+	{
+		buff_size[num] = c;
+		num++;
+	}
+	return (1);
+}
+
+/**
+ * _puts - a function that prints a string to stdout
+ * @str: pointer to the string to print
+ * Return: number of chars written
+ */
+int _puts(char *str)
+{
+	register int num;
+
+	for (num = 0; str[num]; num++)
+		_putchar(str[num]);
+	return (num);
 }
