a38cf32bdc4cf538e2dcf05545cb9e6476eda15a
Kizito24 <kizitochiazor@gmail.com>
2023-01-26 22:21:38 -0800

    Task fix

--- _printf_i.c
+++ _printf_i.c
@@ -2,7 +2,7 @@
 
 /**
  * _printf_i - a function that prints a decimal number in base 10 to stdout.
- * @i: the base 10 decimal to print
+ * @i: the base 10 decimal to print.
  * Return: the total number of the decimals
  */
 int _printf_i(va_list i)
